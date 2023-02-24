e161ba1ee35b94f3a7edfd820613159aa8a31d23
Eray Sahin <eraysahinmoon@gmail.com>
2023-02-24 12:07:44 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// setup our requires
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// multer requires a few options to be setup to store files with file extensions
+// by default it won't store extensions for security reasons
+const storage = multer.diskStorage({
+  destination: "./public/photos/",
+  filename: function (req, file, cb) {
+    // we write the filename as the current date down to the millisecond
+    // in a large web service this would possibly cause a problem if two people
+    // uploaded an image at the exact same time. A better way would be to use GUID's for filenames.
+    // this is a simple example.
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+// tell multer to use the diskStorage function for naming files instead of the default.
+const upload = multer({ storage: storage });
